bf0b4445345d3fd165baef2a7e1ca0ef6f466566
OEspejol <100763991+OEspejol@users.noreply.github.com>
2022-09-19 10:12:51 +0200

    Add files via upload

---
+++ controlador.js
@@ -0,0 +1,11 @@
+var edad = 19;
+
+if(edad >=18){
+    console.log("Aqui puedes entrar champion.");
+}
+else if((edad >20) && (edad < 70)){
+    console.log("Aqui puedes entrar champion. Felidades tt");
+}
+else{
+    console.log("Aqui no entras");
+}
